42f5ec45bab753b68d669dde4b45c181685569af
Akramov_Nikita <akramovnekit@mail.ru>
2023-11-28 23:04:18 +0300

    add ! to main

--- main/read.cpp
+++ main/read.cpp
@@ -13,71 +13,64 @@ try
     {
       f >> str;
       logic::And* temp = new logic::And;
-      sheme[str] = temp;
+      sheme.insert({str, temp});
     }
     else if (str == "or")
     {
       f >> str;
       logic::Or* temp = new logic::Or;
-      sheme[str] = temp;
+      sheme.insert({str, temp});
     }
     else if (str == "src")
     {
       f >> str;
       logic::Src* temp = new logic::Src;
-      sheme[str] = temp;
+      sheme.insert({str, temp});
     }
     else if (str == "out")
     {
       f >> str;
       cout << str << " " << sheme[str]->get_value() << "\n";
     }
-    else if (str == "out-")
-    {
-      cout << "---------\n";
-    }
-    else if (sheme.find(str) != sheme.end())
-    {
-      string name = str;
-      f >> str;
-      if (str == ">>")
-      {
-        f >> str;
-        if (sheme.find(str) != sheme.end())
-        {
-          logic::Element* p = sheme[name];
-          logic::Element* p2 = sheme[str];
-          *(p) >> *(p2);
-        }
-        f >> str;
-        while (str == "," && f)
-        {
-          f >> str;
-          if (sheme.find(str) != sheme.end())
-            *(sheme[name]) >> *(sheme[str]);
-          f >> str;
-        }
-      }
-      else if (str == "<<")
-      {
-        f >> str;
-        if (sheme.find(str) != sheme.end())
-          *(sheme[name]) << *(sheme[str]);
-        f >> str;
-        while (str == "," && f)
-        {
-          f >> str;
-          if (sheme.find(str) != sheme.end())
-            *(sheme[name]) << *(sheme[str]);
-          f >> str;
-        }
-      }
-    }
     else if (str == "!")
     {
       f >> str;
       if (sheme.find(str) != sheme.end())
         !(*(sheme[str]));
+    }
+    else if (str == "out-")
+      cout << "---------\n";
+    else if (sheme.find(str) != sheme.end())
+    {
+      cout << '-' << endl;
+      string name = str;
+      f >> str;
+      bool is_left = str == "<<" ? true : false;
+      while (str != ".")
+      {
+        f >> str;
+        if (str == "!")
+        {
+          f >> str;
+          if (sheme.find(str) != sheme.end())
+          {
+            if (is_left)
+              *(sheme[name]) << !*(sheme[str]);
+            else
+              *(sheme[name]) >> !*(sheme[str]);
+          }
+        }
+        else
+        {
+          if (sheme.find(str) != sheme.end())
+          {
+            if (is_left)
+              *(sheme[name]) << *(sheme[str]);
+            else
+              *(sheme[name]) >> *(sheme[str]);
+          }
+        }
+      }
     }
   }
   f.close();
