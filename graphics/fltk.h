36dfdb4f9b1d545faf165c7e2a97c0b138e054a0
sh1buya <yasyachizh@mail.ru>
2023-12-04 17:43:57 +0300

    niyaz's fltk.h must be used

--- graphics/fltk.h
+++ graphics/fltk.h
@@ -1,8 +1,8 @@
 #ifndef MY_FLTK_H
 #define MY_FLTH_H
-#include "graph_elems/graph_elems.h"
 #include <FL/Fl.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
 #include <FL/Fl_Counter.H>
 #include <FL/Fl_Light_Button.H>
 #include <FL/Fl_Menu_Bar.H>
@@ -10,42 +10,71 @@
 #include <FL/Fl_Menu_Item.H>
 #include <FL/Fl_Menu_Window.H>
 #include <FL/Fl_Scroll.H>
+#include <FL/Fl_Scrollbar.H>
 #include <FL/Fl_Widget.H>
 #include <FL/Fl_Window.H>
-#include <FL/fl_draw.H>
-#include <cmath>
 #include <iostream>
 
-class MyBox : public Fl_Box
+// #include "logicmap.h"
+// #include "logicwindow.h"
+// #include "mapgroup.h"
+
+int choise (int a, int b, int c);
+
+void add_butt (Fl_Widget*, void*);
+
+void add_butt2 (Fl_Widget*, void*);
+
+template <class T> void add_elem (Fl_Widget*, void*);
+
+// void resize_mapgroup (Fl_Widget*, void*);
+
+class MapGroup : public Fl_Group
 {
+  Fl_Menu_Item* menu;
+  bool moving_left = 0;
+  int x_, y_, x_max, y_max, x_min, y_min;
+  Fl_Widget* last_pused;
+
 public:
-  MyBox(int x, int y, int w, int h, const char* l)
-      : Fl_Box{FL_DOWN_BOX, x, y, w, h, l} {};
+  MapGroup(int x, int y, int w, int h, const char* l = 0);
 
-  int handle (int x) override;
+  int handle (int event) override;
 };
-
-void add_butt (Fl_Widget*, void* v);
-
-void add_butt2 (Fl_Widget*, void* v);
 
 class LogicMap : public Fl_Scroll
 {
-  Fl_Menu_Item* menu;
+  MapGroup* map_group;
 
 public:
   LogicMap(int x, int y, int w, int h, const char* l = 0);
 
   int handle (int x) override;
+
+  MapGroup* map () { return map_group; }
 };
 
-class LogicBox : Fl_Box
+class ElemList : public Fl_Scroll
+{
+  MapGroup* mapgroup;
+  Fl_Group* ElemGroup;
+  int gap_y, gap_x, button_w, button_h;
+
+public:
+  ElemList(int x, int y, int w, int h, MapGroup* mapgroup);
+
+  MapGroup* map () { return mapgroup; }
+};
+
+template <class T> class CreateButton : public Fl_Button
 {
 public:
-  LogicBox(int x, int y, int w, int h, const char* l = 0)
-      : Fl_Box{x, y, w, h, l} {
+  CreateButton(int x, int y, int w, int h, const char* l = /*T::type*/ 0)
+      : Fl_Button{x, y, w, h, l}
+  {
+  }  // надо сделать поле type с названием типа
 
-        };
+  int handle (int event);
 };
 
 class LogicWindow : public Fl_Window
@@ -59,40 +88,21 @@ class LogicWindow : public Fl_Window
 public:
   LogicWindow() : Fl_Window{50, 50, 900, 620, "LOGIC"}
   {
-    // верхнее меню
     menu_bar = new Fl_Menu_Bar{0, 0, 900, 20};
     menu_bar->add("file");
-
-    // границы поля
     map_box = new Fl_Box{FL_BORDER_BOX, 5, 25, 690, 590, ""};
-    // поле
+
     map = new LogicMap{10, 30, 680, 580};
-    // увеличили поле
-    new Fl_Group{10, 30, 1000, 1000, ""};
     map->end();
-    // граница правого списка элементов
+
     elems_box = new Fl_Box{FL_BORDER_BOX, 700, 25, 195, 590, ""};
-
-    // контейнер элементов
-    int elems_x = 705;
-    int elems_y = 30;
-    int elems_w = 185;
-    int elems_h = 580;
-    /*
-    elems = new Fl_Scroll{705, 30, 185, 580};
-    */
-    elems = new Fl_Scroll{elems_x, elems_y, elems_w, elems_h};
-    elems->color(FL_GREEN);
-    // Fl_Box*and = new Fl_Box{};
-    int size = 50;
-
+    elems = new ElemList{705, 30, 185, 580, map->map()};
     new graph::Not{elems_x + (elems_w - size) / 2, elems_y + 30, size};
     new graph::And{elems_x + (elems_w - size) / 2, elems_y + 30 * 2 + size,
                    size};
     new graph::Or{elems_x + (elems_w - size) / 2,
                   elems_y + 30 * 3 + 2 * size, size};
     elems->end();
-    elems->type(Fl_Scroll::VERTICAL_ALWAYS);
     color(FL_MAGENTA);
   }
 };
